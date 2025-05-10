a027ff9542adae2c234e9b862a4031d8a27eaa28
Shahd132 <162301748+Shahd132@users.noreply.github.com>
2025-05-10 15:17:18 +0300

    Update UserInfo.java

--- src/UserInfo.java
+++ src/UserInfo.java
@@ -1,9 +1,41 @@
-
+/**
+ * Stores additional information about a user such as full name,
+ * contact details, address, and language preference.
+ */
 public class UserInfo 
 {
-    protected String fullName;
-    protected String phoneNumber;
-    protected String address;
-    protected String preferredLanguage;
+
+    /** The full name of the user. */
+    // private String fullName;
+
+    /** The phone number of the user. */
+    private String phoneNumber;
+
+    // Constructor
+    public UserInfo( String phoneNumber) 
+    {
+        // this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters
+    // public String getFullName() {
+    //     return fullName;
+    // }
+
+    public String getPhoneNumber() 
+    {
+        return phoneNumber;
+    }
+
+    // Setters
+    // public void setFullName(String fullName) {
+    //     this.fullName = fullName;
+    // }
+
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
 }
 
